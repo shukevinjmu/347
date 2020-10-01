48679ea071719d53896c71c4cfc8c0fdd0ba1dbf
Kevin Shu <61431892+shukevin@users.noreply.github.com>
2020-10-01 18:38:41 -0400

    blog4 button

--- blog4.js
+++ blog4.js
@@ -1,6 +1,11 @@
 captchaVerify = document.getElementById("captcha-verify");
+submit = document.getElementById("submit");
 
-if(grecaptcha.getResponse() != 0) {
-    captchaVerify.innerHTML = "Congratulations! you are not a robot.";
-    alert("This is doing something");
-}
+
+
+submit.addEventListener('click', () =>{
+    if(grecaptcha.getResponse() != 0) {
+        captchaVerify.innerHTML = "Congratulations! you are not a robot.";
+        alert("This is doing something");
+    }
+} )
