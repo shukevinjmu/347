3929b021d38dc8c2755d4bd12672c8e8a00d274a
Kevin Shu <61431892+shukevin@users.noreply.github.com>
2020-10-01 18:26:22 -0400

    blog 4 edit

--- blog4.js
+++ blog4.js
@@ -1,5 +1,5 @@
 captchaVerify = document.getElementById("captcha-verify");
 
-if(grecaptcha.getResponse() ==  "success") {
+if(grecaptcha.getResponse() != 0) {
     captchaVerify.innerHTML = "Congratulations! you are not a robot.";
 }
