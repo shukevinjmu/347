412a83a96cce44e9e1dd64e315c739c17ad2105d
Kevin Shu <61431892+shukevin@users.noreply.github.com>
2020-10-01 18:28:21 -0400

    blog4

--- blog4.js
+++ blog4.js
@@ -2,4 +2,5 @@ captchaVerify = document.getElementById("captcha-verify");
 
 if(grecaptcha.getResponse() != 0) {
     captchaVerify.innerHTML = "Congratulations! you are not a robot.";
+    alert("This is doing something");
 }
