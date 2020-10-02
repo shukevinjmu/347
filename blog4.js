94f8c505bbca0fc88797712e57483367080de122
Kevin Shu <61431892+shukevin@users.noreply.github.com>
2020-10-01 21:09:08 -0400

    blog 4

--- blog4.js
+++ blog4.js
@@ -6,5 +6,7 @@ submit = document.getElementById("submit");
 submit.addEventListener('click', () =>{
     if(grecaptcha.getResponse() !== '') {
         captchaVerify.innerHTML = "Congratulations! you are not a robot.";
+        confetti.start();
     }
+
 } )
