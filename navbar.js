62e4ccd3e6cd650cc359f13942f5212b67575dcd
Kevin Shu <61431892+shukevin@users.noreply.github.com>
2020-10-08 23:04:23 -0400

    blog5

--- navbar.js
+++ navbar.js
@@ -1,3 +1,38 @@
 fetch('navbar.html')
   .then(data => data.text())
   .then(html => document.getElementById('navbar-load').innerHTML = html);
+  const sidePanelButton = document.getElementById("side-panel-button")
+  const sidePanel = document.getElementById("panel");
+
+
+  sidePanelButton.addEventListener("click", () =>{
+      if (window.matchMedia('screen and (max-width: 1200px)').matches) {
+          if(sidePanel.style.width > 1 + '%'){
+              sidePanel.style.width = 0;
+              sidePanel.style.height = 100 + '%';
+              sidePanelButton.style.color = 'whitesmoke';
+              sidePanel.style.display = 'none';
+          } else{
+              sidePanel.style.display = 'block';
+              sidePanel.style.height = 30 + '%';
+              sidePanel.style.width = 100 + '%';
+              sidePanelButton.style.color = '#259a7a';
+          }
+  
+      } else {
+          if(sidePanel.style.width > 0 + 'vw'){
+              sidePanel.style.display = 'block';
+              sidePanel.style.width = 0;
+              sidePanel.style.height = 100 + '%';
+              sidePanelButton.style.color = 'whitesmoke';
+          } else{
+              sidePanel.style.display = 'block';
+              sidePanel.style.width = 15 + 'vw';
+              sidePanel.style.height = 100 + '%';
+              sidePanelButton.style.color = '#259a7a';
+          }
+      }
+  
+  
+  });
+  
