1eb7946677129f9814b6857dfabe1c7841e7c56e
Kevin Shu <61431892+shukevin@users.noreply.github.com>
2020-10-08 22:47:19 -0400

    blog5

--- navbar.js
+++ navbar.js
@@ -4,34 +4,38 @@ fetch('navbar.html')
 
   const sidePanel = document.getElementById("panel");
   const sidePanelButton = document.getElementById("side-panel-button")
-  
+  sidePanelButton.addEventListener('click' , () =>{
+    if (window.matchMedia('screen and (max-width: 1200px)').matches) {
+        if(sidePanel.style.width > 1 + '%'){
+            sidePanel.style.width = 0;
+            sidePanel.style.height = 100 + '%';
+            sidePanelButton.style.color = 'whitesmoke';
+            sidePanel.style.display = 'none';
+        } else{
+            sidePanel.style.display = 'block';
+            sidePanel.style.height = 30 + '%';
+            sidePanel.style.width = 100 + '%';
+            sidePanelButton.style.color = '#259a7a';
+        }
+
+    } else {
+        if(sidePanel.style.width > 0 + 'vw'){
+            sidePanel.style.display = 'block';
+            sidePanel.style.width = 0;
+            sidePanel.style.height = 100 + '%';
+            sidePanelButton.style.color = 'whitesmoke';
+        } else{
+            sidePanel.style.display = 'block';
+            sidePanel.style.width = 15 + 'vw';
+            sidePanel.style.height = 100 + '%';
+            sidePanelButton.style.color = '#259a7a';
+        }
+    }
+
+  })
+
   function openPanel(){
-      if (window.matchMedia('screen and (max-width: 1200px)').matches) {
-          if(sidePanel.style.width > 1 + '%'){
-              sidePanel.style.width = 0;
-              sidePanel.style.height = 100 + '%';
-              sidePanelButton.style.color = 'whitesmoke';
-              sidePanel.style.display = 'none';
-          } else{
-              sidePanel.style.display = 'block';
-              sidePanel.style.height = 30 + '%';
-              sidePanel.style.width = 100 + '%';
-              sidePanelButton.style.color = '#259a7a';
-          }
-  
-      } else {
-          if(sidePanel.style.width > 0 + 'vw'){
-              sidePanel.style.display = 'block';
-              sidePanel.style.width = 0;
-              sidePanel.style.height = 100 + '%';
-              sidePanelButton.style.color = 'whitesmoke';
-          } else{
-              sidePanel.style.display = 'block';
-              sidePanel.style.width = 15 + 'vw';
-              sidePanel.style.height = 100 + '%';
-              sidePanelButton.style.color = '#259a7a';
-          }
-      }
+      
   
   
       
